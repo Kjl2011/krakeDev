de4a18b09b57a8dcc3fea372f5b084338c8226b1
Kjl2011 <juandiego0959@gmail.com>
2024-01-24 07:52:57 -0500

    el boton modifica la imagen

--- Ejercicio Git/ejercicio.js
+++ Ejercicio Git/ejercicio.js
@@ -1,3 +1,7 @@
 saludar=function(){
+    let cmpImagenSaludo
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./Imagenes/bailarines.gif"
     console.log("ingresa a saludar");
+
 }

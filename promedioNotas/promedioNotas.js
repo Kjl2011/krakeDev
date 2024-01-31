f4bf2ef5494ddc9f3edb601595be0a5a01ddf27b
Kjl2011 <juandiego0959@gmail.com>
2024-01-31 08:55:42 -0500

    Se realiza los cambios respectivos de promedios con los textos y los giffs

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -1,17 +1,33 @@
 mostrarPromedio=function(){
     let mostrarPromedio;
     let promedioConDosDecimales;
+    let promedio;
     mostrarPromedio=calcularPromedioNotas();
     promedioConDosDecimales=mostrarPromedio.toFixed(2);
-    cambiarTexto("lblResultado",promedioConDosDecimales);
+    promedio=promedioConDosDecimales;
+    mostrarGif(promedio);
+    cambiarTexto("lblResultado",promedio);
+    if(promedio>0 && promedio<5){
+        cambiarTexto("lblResultado","REPROBASTE");
+    }else if(promedio>=5 && promedio<=8){
+        cambiarTexto("lblResultado","Buen Trabajo");
+    }else if(promedio>8 && promedio<=10){
+        cambiarTexto("lblResultado","Excelente");
+    }
+}
+mostrarGif=function(numero){
+    if(numero>=5 && numero<=8){
+        cambiarImagen("imgGif","buentrabajo.gif");
+    }else if(numero>8 && numero<=10){
+        cambiarImagen("imgGif","excelente.gif");
+    }
 }
 calcularPromedioNotas=function(){
     let nota1;
     let nota2;
     let nota3;
     let promedio;
-    let promedioDosDecimales;
-    let promedioTotal;
+
 //recupero los numeros como float
     nota1=recuperarFlotante("lblN1");
     nota2=recuperarFlotante("lblN2");
@@ -20,3 +36,4 @@ calcularPromedioNotas=function(){
     promedio=calcularPromedio(nota1,nota2,nota3);
     return promedio;
 }
+
